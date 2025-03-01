9a9e80dd9290c014e2d19ced096539012ddbe62c
Andrew Numrich <numricha@mymacewan.ca>
2025-03-01 11:38:15 -0700

    Started modal layout

--- ets/app/page.tsx
+++ ets/app/page.tsx
@@ -4,10 +4,19 @@ import Map from "@/app/components/map";
 export default function Home() {
   return (
     <div>
-      <main>
-        <div id="map">
+      <main className="place-content-center">
+        <div id="overlay" className="-z-99 relative w-full place-content-end">
+          <footer className="bg-white">
+            YOOOO
+          </footer>
+        </div>
+
+        <div id="map" className="z-1 relative">
           <Map></Map>
         </div>
+
+
+
       </main>
     </div>
   );
