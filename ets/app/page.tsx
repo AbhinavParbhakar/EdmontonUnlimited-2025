f2a29d1f7edccb8d27d57efb4da93bd3121c1dcc
Andrew Numrich <numricha@mymacewan.ca>
2025-03-01 12:13:02 -0700

    Modal layer working

--- ets/app/page.tsx
+++ ets/app/page.tsx
@@ -3,21 +3,15 @@ import Map from "@/app/components/map";
 
 export default function Home() {
   return (
-    <div>
-      <main className="place-content-center">
-        <div id="overlay" className="-z-99 relative w-full place-content-end">
-          <footer className="bg-white">
+    <div className="place-content-center">
+        <div id="overlay" className="-z-0 absolute top-50 w-full h-full bg-amber-50">
+          <div className="">
             YOOOO
-          </footer>
+          </div>
         </div>
-
-        <div id="map" className="z-1 relative">
+        <div id="map" className="w-full h-full absolute -z-99">
           <Map></Map>
         </div>
-
-
-
-      </main>
     </div>
   );
 }
